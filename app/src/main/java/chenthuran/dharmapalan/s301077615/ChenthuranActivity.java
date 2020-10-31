fcf2e357c4caa824023b906f3203e5e2bd49e549
chenthurand <71531471+chenthurand@users.noreply.github.com>
2020-10-31 08:57:41 -0400

    first commit

---
+++ app/src/main/java/chenthuran/dharmapalan/s301077615/ChenthuranActivity.java
@@ -0,0 +1,14 @@
+package chenthuran.dharmapalan.s301077615;
+//Chenthuran Dharmapalan 301077615
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChenthuranActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
